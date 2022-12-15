6c43fc14dfcc577fe2d5434e917063e0c5c00f4b
Aykut Baştuğ <aykut.bastug@codeove.com>
2022-12-15 14:17:57 +0300

    döngüler

---
+++ Console Apps/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz:");
+
+            //try
+            //{
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //}
+            //catch (Exception ex)
+            //{
+            //    //Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+
+            try
+            {
+                int sayi = Convert.ToInt32(Console.ReadLine());
+            }
+            catch 
+            {
+                ;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Console Apps/ConsoleApp4/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello, World!");
+            //}
+
+
+            //for (char i = 'A'; i < 'Z'; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.WriteLine("Bir sayı girin:");
+            //int sayi = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < sayi; i++)
+            //{
+            //    Console.WriteLine($"{i} sayısı { (i % 2 == 0 ? "çifttir" : "tektir") }");
+
+            //}
+
+
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 3)
+            //        continue;
+
+            //    Console.WriteLine(i);
+
+
+            //    if (i == 6)
+            //        break;
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+
+
+                for (int a = 50; a < 55; a++)
+                {
+                    Console.WriteLine(a * i);
+                }
+            }
+        }
+    }
+}
