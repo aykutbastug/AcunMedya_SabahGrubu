ab934a9eaf8f0fb92057adfd5fe710dbf52a29a5
Aykut Baştuğ <aykut.bastug@codeove.com>
2022-12-16 13:15:45 +0300

    methot örnekleri

--- Console Apps/ConsoleApp6/Program.cs
+++ Console Apps/ConsoleApp6/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    DenemeMetot();                
+            //}
+
+            //DenemeMetot();
+            //DenemeMetot("deneme mesaj", 123);
+
+            //DenemeMetot();
+            //DenemeMetot("aykut baştuğ");
+            //DenemeMetot("aykut baştuğ");
+
+            //EkranaYaz();
+
+            int sonuc = SayilariTopla();
+            Console.WriteLine(sonuc);
+            Console.WriteLine(SayilariTopla());
+            EkranaYaz(sonuc.ToString());
+            EkranaYaz(SayilariTopla().ToString());
+        }
+
+        //static void DenemeMetot()
+        //{
+        //    Console.WriteLine("Hello, World!");
+        //}
+
+        static void DenemeMetot(string mesaj, int mesaj2)
+        {
+            Console.WriteLine(mesaj);
+        }
+
+        static void DenemeMetot(string mesaj = "deneme")
+        {
+            EkranaYaz(mesaj);
+        }
+
+        static void EkranaYaz(string mesaj)
+        {
+            Console.WriteLine(mesaj);
+        }
+
+        static int SayilariTopla()
+        {
+
+
+            return 5 + 4;
         }
     }
 }
