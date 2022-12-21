4ba8c88d768f0099c3fc22692d46d8614b13aaa6
Aykut Baştuğ <aykut.bastug@codeove.com>
2022-12-21 13:27:24 +0300

    winform örnek

---
+++ Windows Form Apps/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 45);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adı Soyadı :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(248, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 161);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "AYKUT";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(597, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tıkla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(248, 222);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(306, 161);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "AYKUT";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(248, 403);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(306, 161);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "AYKUT";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 45F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1460, 1059);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Form Uygulaması";
+            this.Load += new System.EventHandler(this.FormAcildigindaCalis);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ Windows Form Apps/WinFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAcildigindaCalis(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Form açılıyor");
+
+            //label1.TextAlign = ContentAlignment.MiddleRight;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Text = this.Width + "X" + this.Height;
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çift tıklama yapıldı");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(textBox1.Text);
+
+
+
+            //if (textBox3.Visible == true)
+            //    textBox3.Visible = false;
+            //else
+            //    textBox3.Visible = true;
+
+
+
+            //if (textBox3.Visible)
+            //    textBox3.Visible = false;
+            //else
+            //    textBox3.Visible = true;
+
+
+            //textBox3.Visible = textBox3.Visible ? false: true;
+
+            textBox3.Visible = !textBox3.Visible;
+        }
+    }
+}
