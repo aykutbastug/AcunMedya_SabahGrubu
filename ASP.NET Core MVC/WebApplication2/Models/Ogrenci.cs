eb64f2f29f8d6132ad6e6d4eabafd2d089121023
Aykut Baştuğ <aykut.bastug@codeove.com>
2023-01-05 14:20:26 +0300

    razor örnekleri

---
+++ ASP.NET Core MVC/WebApplication2/Models/Ogrenci.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Ogrenci
+    {
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Bolum { get; set; }
+        public string Telefon { get; set; }
+    }
+}
