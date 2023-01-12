12203f43dc4737672dc99b4f165ad2992f2b6a1f
Aykut Baştuğ <aykut.bastug@codeove.com>
2023-01-12 14:27:22 +0300

    mvc örnek

---
+++ ASP.NET Core MVC/WebApplication4/Controllers/MusteriController.cs
@@ -0,0 +1,53 @@
+﻿using MFramework.Services.FakeData;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class MusteriController : Controller
+    {
+        static List<Musteri> Musteriler = new List<Musteri>();
+        public IActionResult Index()
+        {
+            if (Musteriler.Count == 0)
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    Musteriler.Add(new Musteri() { 
+                        Id = i,
+                        EFaturaMukellefi = BooleanData.GetBoolean(),
+                        KayitTarihi = DateTimeData.GetDatetime(),
+                        Telefon = PhoneNumberData.GetPhoneNumber(),
+                        Unvan = NameData.GetCompanyName(),
+                        ToplamAlacak = NumberData.GetDouble(),
+                        ToplamBorc = NumberData.GetDouble()
+                    }); 
+                }
+            }
+
+            return View(Musteriler);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Musteri musteri)
+        {
+            Musteriler.Add(musteri);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Musteri musteri = Musteriler.FirstOrDefault(x => x.Id == id);
+
+            return View(musteri);
+        }
+    }
+}
---
+++ ASP.NET Core MVC/WebApplication4/Models/Musteri.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace WebApplication4.Models
+{
+    public class Musteri
+    {
+        [DisplayName("No")]
+        public int Id { get; set; }
+
+        [DisplayName("Kayıt Tarihi")]
+        public DateTime KayitTarihi { get; set; } = DateTime.Now;
+
+        [DisplayName("Ünvan")]
+        public string Unvan { get; set; }
+
+        [DisplayName("Telefon")]
+        public string Telefon { get; set; }
+
+        [DisplayName("E-Fatura")]
+        public bool EFaturaMukellefi { get; set; }
+
+        [DisplayName("Toplam Alacak")]
+        public double ToplamAlacak { get; set; }
+
+        [DisplayName("Toplam Borç")]
+        public double ToplamBorc { get; set; }
+    }
+}
